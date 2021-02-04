aa279c4c6647f3d4be5de53314a7b95f7661ed7d
Paola <paola.martinez2907@gmail.com>
2021-02-04 17:20:35 -0600

    Modulo de login agregado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You ar logged in!') }}
                 </div>
             </div>
         </div>
